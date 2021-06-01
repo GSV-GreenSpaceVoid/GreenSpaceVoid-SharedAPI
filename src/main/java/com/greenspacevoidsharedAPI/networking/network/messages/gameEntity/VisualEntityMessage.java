a586a35c71b0ccfb06ea1f4c2275002c35e3e197
GSV-GreenSpaceVoid <79030193+GSV-GreenSpaceVoid@users.noreply.github.com>
2021-06-01 01:58:47 -0500

    Broken crap

---
+++ src/main/java/com/greenspacevoidsharedAPI/networking/network/messages/gameEntity/VisualEntityMessage.java
@@ -0,0 +1,16 @@
+package com.greenspacevoidsharedAPI.networking.network.messages.gameEntity;
+
+public class VisualEntityMessage {
+    String name;
+    int x, y;
+    float dir;
+    int ID, UUID;
+
+
+
+
+
+
+
+
+}

37940af66a372019f9ac83d45c3db9a6c30b7173
GSV-GreenSpaceVoid <79030193+GSV-GreenSpaceVoid@users.noreply.github.com>
2021-06-15 23:05:03 -0500

    Added new API Messages (Requires Kryonet implementation)

---
+++ src/main/java/com/greenspacevoidsharedAPI/networking/network/messages/generalInfo/InformationMessages.java
@@ -0,0 +1,40 @@
+package com.greenspacevoidsharedAPI.networking.network.messages.generalInfo;
+
+public class InformationMessages {
+
+
+
+    public static class WarnMessage{
+        public String message;
+        public WarnMessage(){};
+    }
+
+    public static class CombatMessage{
+        public String message;
+        public CombatMessage(){};
+    }
+
+    public static class FinancialWarnMessage{
+        public String message;
+        public FinancialWarnMessage(){};
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
